671e0910931faf937801fec43f4545f3d7847b00
Nole <oscar8nole@gmail.com>
2021-10-27 19:54:14 +0200

    Creada la estructura primcipal

---
+++ src/comonent/MoviesGrid/MoviesGrid.jsx
@@ -0,0 +1,12 @@
+import { MovieCard } from '../MoviesCard/MoviesCard'
+import movies from '../../movies.json'
+import styles from "./MoviesGrid.module.css"
+
+export function MoviesGrid() {
+    return (
+        <ul className={styles.movieGrid}>
+            {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </ul>
+    )}
